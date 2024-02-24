e38fd9b949db31c2c00e4660797c78bccba83a2e
itisover <143436308+itisover@users.noreply.github.com>
2024-02-24 18:53:15 +0000

    Snail and Slime move correctly, deal damage and die

--- Scenes/Slime/Slime.gd
+++ Scenes/Slime/Slime.gd
@@ -1,33 +1,65 @@
 extends CharacterBody2D
 
-@export var gravity = 1000
+@export var gravity = 2000
 @export var speed = 1000
-@export var jump_speed = -1000
+@export var jump_speed = -1500
 @export var jump_cooldown = 5
+@export var damage_to_player = 1
+
 const UP = Vector2(0,-1)
+
+var animatedsprite
+var start_jump_timer = 0.4
+var start_jump= true
+var start_start_jump_timer = true
+
+var airborne = false
 var player
 var attack = false
 var jump_timer = false
+var dead = false 
+
+func _ready():
+	$AnimatedSprite2D.animation = "slime_default"
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta):
+	if dead:
+		return
+		
 	velocity.y += gravity * delta
 	
-
+	if airborne and $AnimatedSprite2D.animation != "slime_death":
+		$AnimatedSprite2D.animation = "slime_jump"
+		
+	if airborne and is_on_floor():
+		$SlimeMoveSound.play()
+		airborne = false
+	
 	if attack and jump_timer and is_on_floor():
-		player = get_node("../Player")
-		var direction = (player.position - self.position).normalized()
-		velocity.y += jump_speed
-		velocity.x = direction.x * speed
-		jump_timer = false
-		get_node("JumpCooldown").start(jump_cooldown)
-		get_node("AnimatedSprite2D").play("slime_jump")
+		# If starting a jump, play the animation and start the timer to wait until the animation finishes
+		if start_jump:
+			$AnimatedSprite2D.animation = "slime_start_jump"
+			if start_start_jump_timer:
+				get_node("StartJumpTimer").start(start_jump_timer)
+				start_start_jump_timer = false
+		else:
+			player = get_node("../Player")
+			var direction = (player.position - self.position).normalized()
+			velocity.y += jump_speed
+			velocity.x = direction.x * speed
+			jump_timer = false
+			get_node("JumpCooldown").start(jump_cooldown)
 	elif is_on_floor():
-		get_node("AnimatedSprite2D").play("slime_default")
+		$AnimatedSprite2D.animation = "slime_default"
 		velocity.x = 0
-	
+	else:
+		airborne = true 
+
 	move_and_slide()
-	
-func _on_player_detect_body_entered(body):
+
+
+func _on_player_detect_body_entered(body):	
 	if body.name == "Player":
 		attack = true
 
@@ -37,7 +69,35 @@ func _on_player_detect_body_exited(body):
 		attack = false
 
 
-func _on_timer_timeout():
-	print("HELLO")
+func _on_start_jump_timer_timeout():
+	start_jump = false
+
+
+func _on_jump_cooldown_timeout():
 	jump_timer = true
+	start_jump = true
+	start_start_jump_timer = true
+
+
+func _on_player_on_top_body_entered(body):
+	if body.name == "Player":
+		death()
+
+
+func _on_player_collider_body_entered(body):
+	if body.name == "Player":
+		body.damage(damage_to_player)
+
+
+func _on_health_dead():
+	death()
 	
+func death():
+	velocity.x = 0
+	attack = false
+	dead = true
+	$AnimatedSprite2D.animation = "slime_death"
+	$AnimatedSprite2D.play()
+	await $AnimatedSprite2D.animation_finished
+	queue_free()
+
--- Scenes/Snail/Snail.gd
+++ Scenes/Snail/Snail.gd
@@ -1,24 +1,71 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+@export var gravity = 2000
 @export var speed = 100
-var velocity = Vector2.ZERO
-var gravity = 10
+@export var damage_to_player = 1
 
+const UP = Vector2(0,-1)
 
-# Called when the node enters the scene tree for the first time.
+var start_jump_timer = 0.4
+var start_jump = true
+var start_start_jump_timer = true
+var attack = false
+var dead = false
+var player
+
 func _ready():
+	$AnimatedSprite2D.animation = "snail_move"
 	$AnimatedSprite2D.play()
 
 func _physics_process(delta):
-	velocity.x = speed
-	var collision = move_and_collide(velocity * delta)
-	velocity.y += gravity * delta
-	if collision:
-		speed *= -1
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position += velocity * delta 
-	if velocity.x < 0 :
-		$AnimatedSprite2D.flip_h
+	if dead:
+		return
 	
+	velocity.y += gravity * delta
+	velocity.x = speed
+	if attack:
+		player = get_node("../Player")
+		var direction = (player.position - self.position).normalized()
+		velocity.x = direction.x * speed
+		
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+	
+	if $AnimatedSprite2D.animation != "snail_death":
+		$AnimatedSprite2D.animation = "snail_move"
+	
+	move_and_slide()
+
+
+func _on_player_detect_body_entered(body):	
+	if body.name == "Player":
+		attack = true
+
+
+func _on_player_detect_body_exited(body):
+	if body.name == "Player":
+		attack = false
+
+
+func _on_health_dead():
+	death()
+
+
+func _on_player_on_top_body_entered(body):
+	if body.name == "Player":
+		death()
+
+
+func death():
+	velocity.x = 0
+	attack = false
+	dead = true
+	$AnimatedSprite2D.animation = "snail_death"
+	await $AnimatedSprite2D.animation_finished
+	queue_free()
+
+func _on_player_collider_body_entered(body):
+	if body.name == "Player":
+		player.damage(damage_to_player)
