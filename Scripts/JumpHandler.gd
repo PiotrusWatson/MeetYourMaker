4f33d64484aa40f37f995265809b6dbacc990013
piotrus <piotruswatson@gmail.com>
2024-02-24 13:28:05 +0000

    made jumps less frustrating

--- Scripts/JumpHandler.gd
+++ Scripts/JumpHandler.gd
@@ -4,7 +4,10 @@ extends Node2D
 @export var jump_strength_min = 10
 @export var jump_strength_max = 100
 @export var jump_strength_increment = 1
+@export var max_buffer_frames = 30
+var buffer_frames = 0
 
+var is_buffering = false
 @onready var jump_timer = $JumpTimer
 var player_rigidbody
 var jump_strength
@@ -20,7 +23,15 @@ func init(rigidbody: RigidBody2D):
 func charge_jump():
 	jump_timer.start()
 	
-
+func _process(delta):
+	if !is_buffering:
+		return
+	if buffer_frames < max_buffer_frames:
+		if handle_jump():
+			reset_jump()
+		buffer_frames += 1
+		
+	
 func increment_jump():
 	if jump_strength <= jump_strength_max:
 		jump_strength += jump_strength_increment
@@ -30,27 +41,24 @@ func increment_jump():
 func reset_jump():
 	jump_strength = jump_strength_min
 	jump_timer.stop()
-	
-func jump():
+	is_buffering = false
+	buffer_frames = 0
+
+func handle_jump():
 	if grounded:
 		player_rigidbody.apply_central_impulse(Vector2(0, -jump_strength))
+		reset_jump()
+		return true
+	return false
+func jump():
+	is_buffering = true
 	
-	reset_jump()
-	
-
-
 func _on_jump_timer_timeout():
 	increment_jump()
 
 
-
-	
-
-
 func _on_body_entered(body):
 	grounded = true
-
-
 
 func _on_body_exited(body):
 	grounded = false
