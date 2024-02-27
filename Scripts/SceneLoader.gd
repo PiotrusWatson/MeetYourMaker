673b401f5867cac7f04021837dee0b0edd827dd9
piotrus <piotruswatson@gmail.com>
2024-02-27 19:43:38 +0000

    this is the build demonstrated at the game jam - with janky level, working enemies, and a quick placeholder ending

--- Scripts/SceneLoader.gd
+++ Scripts/SceneLoader.gd
@@ -3,6 +3,7 @@ extends Node
 var death_scene = load("res://WorldScenes/death_scene.tscn")
 var game_scene = load("res://WorldScenes/RealWorld.tscn")
 var intro_scene = load("res://WorldScenes/dialogue_scene.tscn")
+var end_scene = load("res://end_scene.gd")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -31,3 +32,9 @@ func _on_reload_pressed():
 func _on_button_pressed():
 	$MainMenuMusic.stop()
 	load_scene(intro_scene)
+
+
+func _on_end_trigger_body_entered(body):
+	if body.has_method("player"):
+		load_scene(end_scene)
+		
--- WorldScenes/Spawner.gd
+++ WorldScenes/Spawner.gd
@@ -1,14 +1,15 @@
-extends Node2D
+extends Area2D
 
 @export var thingToSpawn : PackedScene
 @export var max_spawns = 5
+var active = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func spawn():
-	if get_child_count() - 2 > max_spawns:
+	if get_child_count() - 3 >= max_spawns or !active:
 		return
 	var spawned_thing = thingToSpawn.instantiate()
 	add_child(spawned_thing)
@@ -17,3 +18,8 @@ func spawn():
 
 func _on_timer_timeout():
 	spawn()
+
+
+func _on_body_entered(body):
+	if body.has_method("player"):
+		active = true
